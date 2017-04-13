0ac06f4600206ac47aa5bd84c1cba960944c8f7f
samer <samer_mail_2006@yahoo.com>
2017-04-13 09:33:56 +0300

    add reservation page.

---
+++ Kinect/DB/Reservation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Kinect.DB
+{
+    public class Reservation
+    {
+        public int MovieId { get; set; }
+        public DateTime ReservationTime { get; set; }
+    }
+}
